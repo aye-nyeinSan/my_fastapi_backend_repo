5df619af95140255bd218be69f34c27094e58847
Gloria Win Nyunt <94699079+gloriabee@users.noreply.github.com>
2025-06-02 16:59:59 +0700

    Routes folder setup, and endpoint testing (#4)
    * install required dependencies for sql table
    
    * add db.py and update environment variables
    
    * routes setup and get/post enpoint testing with testing table

---
+++ core/db.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# load .env file
+load_dotenv()
+
+# get variables from environment
+POSTGRES_URL_NON_POOLING=os.getenv("POSTGRES_URL_NON_POOLING")
+POSTGRES_USER=os.getenv("POSTGRES_USER")
+POSTGRES_HOST=os.getenv("POSTGRES_HOST")
+POSTGRES_PORT=os.getenv("POSTGRES_PORT")
+SUPABASE_JWT_SECRET=os.getenv("SUPABASE_JWT_SECRET")
+
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
+
+# DATABASE_URL=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
+DATABASE_URL=POSTGRES_URL_NON_POOLING
+
+engine= create_engine(DATABASE_URL)
+SessionLocal= sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base=declarative_base()
+
+
+
+
---
+++ routes/retrain.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, HTTPException
+import os
+import requests
+
+router= APIRouter()
+
+@router.post("/retrainmodel")
+async def retrain_model():
+    GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+    GITHUB_REPO_OWNER = os.getenv('GITHUB_REPO_OWNER')
+    GITHUB_REPO_NAME = os.getenv('GITHUB_REPO_NAME')
+
+    if not GITHUB_TOKEN:
+        return { "error": "Github token is missing"},500
+    if not GITHUB_REPO_OWNER or not GITHUB_REPO_NAME:
+        return { "error": "Github repository is misconfigured"},500
+    
+    url = f"https://api.github.com/repos/{GITHUB_REPO_OWNER}/{GITHUB_REPO_NAME}/actions/workflows/retrain_model.yml/dispatches"
+    
+    headers = { 
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "X-GitHub-Api-Version": "2022-11-28",
+    "User-Agent": f"{GITHUB_REPO_OWNER}",
+            }
+    
+    try:
+        response = requests.post(
+            url,
+            headers=headers,
+            json={"ref": "main"}
+        )
+        response.raise_for_status()
+        
+        return {"message": "Model retraining started",
+            "github_repo": f"{GITHUB_REPO_OWNER}/{GITHUB_REPO_NAME}",
+            "status_code": response.status_code}
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error dispatching GitHub workflow: {e}")
+        raise HTTPException(status_code=response.status_code, detail=str(e))
